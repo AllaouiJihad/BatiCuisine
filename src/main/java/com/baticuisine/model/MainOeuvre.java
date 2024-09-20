c625793f10a2dc7381caad9c33138e6452b54075
jihad <allaouijihad05@gmail.com>
2024-09-20 12:31:27 +0100

    add mainOeuvre entity

--- src/main/java/com/baticuisine/model/MainOeuvre.java
+++ src/main/java/com/baticuisine/model/MainOeuvre.java
@@ -1,4 +1,48 @@
 package main.java.com.baticuisine.model;
 
+import java.math.BigDecimal;
+
 public class MainOeuvre {
+    private BigDecimal tauxHoraire;
+    private BigDecimal heuresTravail;
+    private BigDecimal productiviteOuvrier;
+
+    public MainOeuvre(BigDecimal tauxHoraire, BigDecimal heuresTravail, BigDecimal productiviteOuvrier) {
+        this.tauxHoraire = tauxHoraire;
+        this.heuresTravail = heuresTravail;
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    public BigDecimal getTauxHoraire() {
+        return tauxHoraire;
+    }
+
+    public void setTauxHoraire(BigDecimal tauxHoraire) {
+        this.tauxHoraire = tauxHoraire;
+    }
+
+    public BigDecimal getHeuresTravail() {
+        return heuresTravail;
+    }
+
+    public void setHeuresTravail(BigDecimal heuresTravail) {
+        this.heuresTravail = heuresTravail;
+    }
+
+    public BigDecimal getProductiviteOuvrier() {
+        return productiviteOuvrier;
+    }
+
+    public void setProductiviteOuvrier(BigDecimal productiviteOuvrier) {
+        this.productiviteOuvrier = productiviteOuvrier;
+    }
+
+    @Override
+    public String toString() {
+        return "MainOeuvre{" +
+                "tauxHoraire=" + tauxHoraire +
+                ", heuresTravail=" + heuresTravail +
+                ", productiviteOuvrier=" + productiviteOuvrier +
+                '}';
+    }
 }
